db6669186ebe524ab5b0592dc6e2ed5f5a1866e7
Fotis Aronis <aronisfotis@gmail.com>
2018-09-08 21:54:33 +0300

    Face Detection + Cascades

---
+++ PictureFaceDetection.py
@@ -0,0 +1,26 @@
+import cv2
+import sys
+
+# Input
+imagePath = sys.argv[1]
+cascPath = "haarcascade_frontalface_default.xml"
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read the image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+
+print("I Found {0} Faces!".format(len(faces)))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("Faces Found", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
